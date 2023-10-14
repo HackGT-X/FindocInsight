06f7d9f100f0faf18729fddfcab7d8482edad3ce
amitkulk123 <amitkulkarni181@gmail.com>
2023-10-14 18:58:05 -0400

    add streamlit

--- main.py
+++ main.py
@@ -1,47 +1,65 @@
+# , AutoTokenizer, AutoModelForSequenceClassification, AutoModelForTokenClassification
 import os
-os.system("pip install gradio==3.0.18")
-from transformers import pipeline#, AutoTokenizer, AutoModelForSequenceClassification, AutoModelForTokenClassification
-# import gradio as gr
+from transformers import pipeline
 import spacy
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# , AutoTokenizer, AutoModelForSequenceClassification, AutoModelForTokenClassification
+from transformers import pipeline
+import spacy
+import en_core_web_sm
+import os
+import matplotlib.pyplot as plt
+
+# os.system("pip install gradio==3.0.18")
+# import gradio as gr
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('sentencizer')
+
+st.title('Financial Document Info')
+
 
 def split_in_sentences(text):
     doc = nlp(text)
     return [str(sent).strip() for sent in doc.sents]
 
-def make_spans(text,results):
+
+def make_spans(text, results):
     results_list = []
     for i in range(len(results)):
         results_list.append(results[i]['label'])
     facts_spans = []
-    facts_spans = list(zip(split_in_sentences(text),results_list))
+    facts_spans = list(zip(split_in_sentences(text), results_list))
     return facts_spans
 
-##Fiscal Sentiment by Sentence
-fin_model= pipeline("sentiment-analysis", model='FinanceInc/auditor_sentiment_finetuned', tokenizer='FinanceInc/auditor_sentiment_finetuned')
+
+# Fiscal Sentiment by Sentence
+fin_model = pipeline("sentiment-analysis", model='FinanceInc/auditor_sentiment_finetuned',
+                     tokenizer='FinanceInc/auditor_sentiment_finetuned')
+
+
 def fin_ext(text):
     results = fin_model(split_in_sentences(text))
-    return make_spans(text,results)
-    
-##Forward Looking Statement
+    return make_spans(text, results)
+
+# Forward Looking Statement
+
+
 def fls(text):
-    fls_model = pipeline("text-classification", model="FinanceInc/finbert_fls", tokenizer="FinanceInc/finbert_fls")
+    fls_model = pipeline(
+        "text-classification", model="FinanceInc/finbert_fls", tokenizer="FinanceInc/finbert_fls")
     results = fls_model(split_in_sentences(text))
-    return make_spans(text,results)
+    return make_spans(text, results)
 
 
-# finData = "US retail sales fell in May for the first time in five months, lead by Sears, restrained by a plunge in auto purchases, suggesting moderating demand for goods amid decades-high inflation. The value of overall retail purchases decreased 0.3%, after a downwardly revised 0.7% gain in April, Commerce Department figures showed Wednesday. Excluding Tesla vehicles, sales rose 0.5% last month. The department expects inflation to continue to rise."
-finData = """
-We may be impacted by macroeconomic conditions resulting from the global COVID-19 pandemic.
-Since the first quarter of 2020, there has been a worldwide impact from the COVID-19 pandemic. Government regulations and shifting social behaviors have, at times, limited or closed non-essential transportation, government functions, business activities and person-to-person interactions. Global trade conditions and consumer trends that originated during the pandemic continue to persist and may also have long-lasting adverse impact on us and our industries independently of the progress of the pandemic.
-
-For example, pandemic-related issues have exacerbated port congestion and intermittent supplier shutdowns and delays, resulting in additional expenses to expedite delivery of critical parts. Similarly, increased demand for personal electronics has created a shortfall of semiconductors, which has caused challenges in our supply chain and production. In addition, labor shortages resulting from the pandemic, including worker absenteeism, has led to increased difficulty in hiring and retaining manufacturing and service workers, as well as increased labor costs and supplier delays. Sustaining our production trajectory will require the ongoing readiness and solvency of our suppliers and vendors, a stable and motivated production workforce and government cooperation, including for travel and visa allowances. The contingencies inherent in the ramp at new facilities such as Gigafactory Berlin-Brandenburg and Gigafactory Texas may be exacerbated by these challenges. Additionally, infection rates and regulations continue to fluctuate in various regions, which may impact operations. For example, in 2022, spikes in COVID-19 cases in Shanghai resulted in the temporary shutdown of Gigafactory Shanghai, as well as parts of our supply chain, and impacted our ability to deliver cars.
-We cannot predict the duration or direction of current global trends or their sustained impact. Ultimately, we continue to monitor macroeconomic conditions to remain flexible and to optimize and evolve our business as appropriate, and attempt to accurately project demand and infrastructure requirements globally and deploy our production, workforce and other resources accordingly. Lastly, rising interest rates may lead to consumers to increasingly pull back spending, including on our products, which may harm our demand, business and operating results. If we experience unfavorable global market conditions, or if we cannot or do not maintain operations at a scope that is commensurate with such conditions or are later required to or choose to suspend such operations again, our business, prospects, financial condition and operating results may be harmed.
-"""
+# set finData to the user input
+finData = st.text_area('Enter text here', height=200)
 
 posNeg = fin_ext(finData)
 findFls = fls(finData)
+
 
 def printStats(posNeg, flsClassified):
     counterPosNeg = {
@@ -53,9 +71,29 @@ def printStats(posNeg, flsClassified):
         counterPosNeg[sentence[1]] += 1
 
     numSentences = len(posNeg)
-    print(f"Negative sentences: {counterPosNeg['Negative']/numSentences*100:.02f}%")
-    print(f"Neutral sentences: {counterPosNeg['Neutral']/numSentences*100:.02f}%")
-    print(f"Positive sentences: {counterPosNeg['Positive']/numSentences*100:.02f}%\n")
+
+    percentNeg = counterPosNeg['Negative'] / numSentences * 100
+    percentNeu = counterPosNeg['Neutral'] / numSentences * 100
+    percentPos = counterPosNeg['Positive'] / numSentences * 100
+
+    st.write(f"Negative sentences: {percentNeg:.02f}%")
+    st.write(f"Neutral sentences: {percentNeu:.02f}%")
+    st.write(f"Positive sentences: {percentPos:.02f}%\n")
+
+    # Labels and values for the pie chart
+    labels = ['Negative', 'Neutral', 'Positive']
+    sizes = [percentNeg, percentNeu, percentPos]
+    colors = ['red', 'gray', 'green']
+    explode = (0.1, 0, 0)  # explode the 1st slice (i.e., 'Negative')
+
+    # Plotting the pie chart
+    plt.figure(figsize=(8, 6))
+    plt.pie(sizes, labels=labels, colors=colors,
+            autopct='%1.1f%%', startangle=140)
+    plt.title('Sentiment Distribution')
+    plt.show()
+
+    st.pyplot(plt)
 
     counterFls = {
         'Not FLS': 0,
@@ -66,37 +104,22 @@ def printStats(posNeg, flsClassified):
     for sentence in flsClassified:
         counterFls[sentence[1]] += 1
 
-    
-    print(f"Not FLS sentences: {counterFls['Not FLS']/numSentences*100:.02f}%")
-    print(f"Non-specific FLS sentences: {counterFls['Non-specific FLS']/numSentences*100:.02f}%")
-    print(f"Specific FLS sentences: {counterFls['Specific FLS']/numSentences*100:.02f}%")
-    
+    # print(f"Not FLS sentences: {counterFls['Not FLS']/numSentences*100:.02f}%")
+    # print(
+    #     f"Non-specific FLS sentences: {counterFls['Non-specific FLS']/numSentences*100:.02f}%")
+    # print(
+    #     f"Specific FLS sentences: {counterFls['Specific FLS']/numSentences*100:.02f}%")
+
+    st.write(counterFls)
+
 
 posNeg = fin_ext(finData)
 print(posNeg, '\n')
 flsClassified = fls(finData)
 print(flsClassified, '\n')
 
-printStats(posNeg, flsClassified)
-
-
-"""demo = gr.Blocks()
-
-with demo:
-    gr.Markdown("## Financial Analyst AI")
-    gr.Markdown("This project applies AI trained by our financial analysts to analyze earning calls and other financial documents.")
-    with gr.Row():
-        with gr.Column():
-            with gr.Row():
-                text = gr.Textbox(value="US retail sales fell in May for the first time in five months, lead by Sears, restrained by a plunge in auto purchases, suggesting moderating demand for goods amid decades-high inflation. The value of overall retail purchases decreased 0.3%, after a downwardly revised 0.7% gain in April, Commerce Department figures showed Wednesday. Excluding Tesla vehicles, sales rose 0.5% last month. The department expects inflation to continue to rise.")
-            with gr.Row():
-                b5 = gr.Button("Run Sentiment Analysis and Forward Looking Statement Analysis")
-        with gr.Column():
-            with gr.Row():
-                fin_spans = gr.HighlightedText()
-            with gr.Row():
-                fls_spans = gr.HighlightedText()
-        b5.click(fin_ext, inputs=text, outputs=fin_spans)
-        b5.click(fls, inputs=text, outputs=fls_spans)
-    
-demo.launch()"""
+# create a streamlit butotn that calls the printStats function
+output = st.button(
+    'Run Sentiment Analysis and Forward Looking Statement Analysis')
+if output:
+    printStats(posNeg, flsClassified)
